f13d45be5d66bea3e59badf260cf7ab59cf83b57
double Z <doubleZ0108@163.com>
2020-03-30 23:51:19 +0800

    :art:Art of Parallax

---
+++ Art of Parallax/parallax.js
@@ -0,0 +1,18 @@
+
+window.onload = function () {
+    let bg = document.getElementById("bg");
+    let moon = document.getElementById("moon");
+    let mountain = document.getElementById("mountain");
+    let road = document.getElementById("road");
+    let text = document.getElementById("text");
+
+    window.addEventListener('scroll', function () {
+        var value = window.scrollY;
+
+        bg.style.top = value * 0.5 + 'px';
+        moon.style.left = -value * 0.5 + 'px';
+        mountain.style.top = -value * 0.15 + 'px';
+        road.style.top = value * 0.15 + 'px';
+        text.style.top = value * 1.0 + 'px';
+    });
+};
