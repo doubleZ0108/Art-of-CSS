e4f74118d2680e6ae84d918e8ac0e60939dde46c
double Z <doubleZ0108@163.com>
2020-04-14 11:36:38 +0800

    :art:art of social media icon

--- Art of Social-Media-Icon/SocialMedianIcon.js
+++ Art of Social-Media-Icon/SocialMedianIcon.js
@@ -1,6 +1,13 @@
 window.onload = function () {
-    $('.first li').click(function () {
-        console.log("hello");
-        $(this).toggleClass("shadow-1");
+    // circle版的样式都在li上
+    $('.circle li').click(function () {
+        $(this).toggleClass("clicked");
+        $(this).toggleClass("fill-color");
+    });
+
+    // ring版特殊样式在a上
+    $('.ring li a').click(function () {
+        $(this).toggleClass("clicked");
+        $(this.parentNode).toggleClass("fill-color");
     });
 };
