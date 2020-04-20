89a32dadebd613ef57cd4ded134a8bcf1b82b1ce
double Z <doubleZ0108@163.com>
2020-04-20 23:33:43 +0800

    :art:art of hover card

---
+++ Art of Hover-Card/HoverCard.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    $('.card').hover(function () {
+        if($(this).hasClass("active")){
+            $('.card .bottom').slideUp(function () {
+                $('.card').removeClass("active");
+            });
+        } else {
+            $('.card').addClass("active");
+            $('.card .bottom').stop().slideDown();
+        }
+    });
+};
