68ec2cac399d5434aa116f273d6fb53f05d13e9a
double Z <doubleZ0108@163.com>
2020-05-12 23:29:03 +0800

    :art:Art of Full Screen Menu

---
+++ Art of FullScreen/FullScreenMenu.js
@@ -0,0 +1,9 @@
+function openDiv() {
+    var theDiv = document.getElementById("myDiv");
+    theDiv.style.transform = "scale(1)";
+}
+
+function closeDiv() {
+    var theDiv = document.getElementById("myDiv");
+    theDiv.style.transform = "scale(0)";
+}
