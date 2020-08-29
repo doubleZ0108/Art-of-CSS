c2f28dfa374373a3e01bb61769474df24f02ab51
double Z <doubleZ0108@163.com>
2020-08-29 20:04:46 +0800

    :alembic:Art of IntersectionObserver

---
+++ Art of IntersectionObserver/IntersectionObserver.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            console.log(entry)
+            if(entry.isIntersecting){
+                entry.target.classList.add('animate')
+                // observer.unobserve(entry.target)
+            } else {
+                entry.target.classList.remove('animate')
+            }
+        })
+    })
+    
+    document.querySelectorAll('mark').forEach(mark => {
+        observer.observe(mark)
+    })
+}
