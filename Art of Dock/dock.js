2a215d8a73456ea5b8f55681af3dce370212eba0
doubleZ <doubleZ0108@163.com>
2021-03-20 19:58:15 +0800

    :art:art of dock

---
+++ Art of Dock/dock.js
@@ -0,0 +1,36 @@
+window.onload = function () {
+    document.querySelectorAll('.dock li').forEach(li => {
+        li.addEventListener('mousemove', e => {
+            let item = e.target
+            let itemRect = item.getBoundingClientRect()
+            
+            // 鼠标hover到某个li上的时候，靠近左侧则 offset -> 0, 靠近右侧则 offset -> 1
+            let offset = Math.abs(e.clientX - itemRect.left) / itemRect.width
+            
+            let prev = item.previousElementSibling || null
+            let next = item.nextElementSibling || null
+
+            let scale = 0.5
+            resetScale()
+
+            if(prev) {
+                prev.style.setProperty('--scale-factor', 1 + scale * Math.abs(offset - 1))
+            }
+
+            item.style.setProperty('--scale-factor', 1 + scale)
+
+            if(next) {
+                next.style.setProperty('--scale-factor', 1 + scale * offset)
+            }
+        })
+    })
+    document.querySelector('.dock').addEventListener('mouseleave', e => {
+        resetScale()
+    })
+}
+
+function resetScale() {
+    document.querySelectorAll('.dock li').forEach(li => {
+        li.style.setProperty('--scale-factor', 1)
+    })
+}
